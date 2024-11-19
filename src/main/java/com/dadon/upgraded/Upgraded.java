41fa238bc2fad54759275f1aca44ef2a179a4977
danie <salimdonald9@gmail.com>
2024-11-19 11:48:55 -0600

    Commit Message

---
+++ src/main/java/com/dadon/upgraded/Upgraded.java
@@ -0,0 +1,15 @@
+package com.dadon.upgraded;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Upgraded implements ModInitializer {
+	public static final String MOD_ID = "upgraded";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/dadon/upgraded/UpgradedClient.java
@@ -0,0 +1,10 @@
+package com.dadon.upgraded;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UpgradedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
